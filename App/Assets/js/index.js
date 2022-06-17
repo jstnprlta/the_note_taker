ae8ba4831e768c806bbcefc10ab6b7310058ead8
Justin <100394905+jstnprlta@users.noreply.github.com>
2022-06-16 19:59:52 -0500

    Index.Js

--- App/Assets/js/index.js
+++ App/Assets/js/index.js
@@ -167,7 +167,7 @@ const renderNoteList = async (notes) => {
   }
 };
 
-// Gets notes from the db and renders them to the sidebar
+// Gets notes from the db and renders the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
 if (window.location.pathname === '/notes') {
